11704081fb8205d32ec6b9467bf8e7d3b8c77721
krithikaweiteredge-glitch <krithika.weiteredge@gmail.com>
2026-09-08 19:32:07 +0530

    Remove GST Certificate from Combo DSC document list

--- src/components/dsc-wizard.tsx
+++ src/components/dsc-wizard.tsx
@@ -67,7 +67,6 @@ const DOCS_COMBO = [
   "PAN Card (mandatory)",
   "Aadhaar Card (for eKYC verification)",
   "Recent passport-size colour photograph",
-  "GST Certificate (extra required for organisation combo DSC)",
 ];
 
 const HIGHLIGHTS = [
